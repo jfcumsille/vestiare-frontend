4caa4477f7518b321e15176e03f2000201c3e5cc
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2020-04-05 02:58:04 -0400

    feat(): allow user to login

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import LogIn from '../views/LogIn.vue';
 import SignUp from '../views/SignUp.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: '/login',
+    component: LogIn,
+  },
   {
     path: '/signup',
     component: SignUp,
--- src/store/index.js
+++ src/store/index.js
@@ -16,6 +16,22 @@ export default new Vuex.Store({
     },
   },
   actions: {
+    logIn({ commit }, formData) {
+      const payload = {
+        email: formData.email,
+        password: formData.password,
+      };
+      const url = process.env.SIGN_IN_ROUTE;
+      axiosAuth.post(url, payload)
+        .then((response) => {
+          const userData = {
+            idToken: response.data.idToken,
+            userId: response.data.localId,
+          };
+          commit('authUser', userData);
+        })
+        .catch((error) => console.log(error));
+    },
     signUp({ commit }, formData) {
       const payload = {
         name: formData.name,
