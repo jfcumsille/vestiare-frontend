dc6b3d9cde327801b9e0f4dce52943bb2d097a9f
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2020-04-16 17:13:56 -0400

    fix(links): fix typo

--- src/views/new-link.vue
+++ src/views/new-link.vue
@@ -118,10 +118,10 @@
                   <div class="flex">
                     <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
                     <div>
-                      <p class="font-bold">Credenciales guardadas exitosamente</p>
+                      <p class="font-bold">Credenciales guardadas con éxito</p>
                       <p class="text-sm">
                         Está información solo será mostrada una vez.
-                        Asegurate de copiar el Access Token
+                        Asegurate de copiar el <b>Access Token</b>
                         </p>
                     </div>
                   </div>
@@ -131,9 +131,6 @@
                     <h3 class="text-lg leading-6 font-medium text-gray-900">
                       Información de las credenciales
                     </h3>
-                    <p class="mt-1 max-w-2xl text-sm leading-5 text-gray-900">
-                      Personal details and application.
-                    </p>
                   </div>
                   <div>
                     <dl>
