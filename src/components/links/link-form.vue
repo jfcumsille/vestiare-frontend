59b38ce35744929d5a1040a370be49b988e49435
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2020-04-17 16:10:39 -0400

    feat(links): add front-end validation

--- src/components/links/link-form.vue
+++ src/components/links/link-form.vue
@@ -9,10 +9,10 @@
         </label>
         <div class="relative">
           <select class="block appearance-none w-full bg-grey-lighter border
-                          border-grey-lighter leading-tight focus:outline-none
-                          text-gray-900 py-3 px-4 pr-8 rounded"
-                  required
-                  v-model="bank">
+                         border-grey-lighter leading-tight focus:outline-none
+                         text-gray-900 py-3 px-4 pr-8 rounded"
+                  v-model="bankName">
+            <option value='' disabled>Selecciona un banco</option>
             <option value='cl_banco_de_chile'>Banco de Chile</option>
             <option value='cl_banco_santander'>Banco Santander</option>
           </select>
@@ -26,6 +26,7 @@
             </svg>
           </div>
         </div>
+        <div v-if="!$v.bankName.required">Este campo es obligatorio</div>
       </div>
       <div class="md:w-1/2 px-3">
         <label class="block uppercase tracking-wide text-gray-900 text-xs
@@ -34,10 +35,10 @@
         </label>
         <div class="relative">
           <select class="block appearance-none w-full bg-grey-lighter border
-                          border-grey-lighter leading-tight focus:outline-none
-                          text-gray-900 py-3 px-4 pr-8 rounded"
-                  required
+                         border-grey-lighter leading-tight focus:outline-none
+                         text-gray-900 py-3 px-4 pr-8 rounded"
                   v-model="holderType">
+            <option value='' disabled>Selecciona un tipo de cuenta</option>
             <option value='individual'>Personas</option>
             <option value='business'>Empresas</option>
           </select>
@@ -51,6 +52,7 @@
             </svg>
           </div>
         </div>
+        <div v-if="!$v.holderType.required">Este campo es obligatorio</div>
       </div>
     </div>
     <div class="-mx-2 md:flex mb-6">
@@ -63,9 +65,9 @@
                       border border-grey-lighter rounded py-3 px-4 leading-tight
                       focus:outline-none"
                 type="text"
-                required
                 placeholder="11111111-1"
                 v-model="rut">
+        <div v-if="!$v.rut.required">Este campo es obligatorio</div>
       </div>
       <div class="md:w-1/2 px-3">
         <label class="block uppercase tracking-wide text-grey-900 text-xs font-bold
@@ -73,23 +75,27 @@
           Password
         </label>
         <input class="appearance-none block w-full bg-grey-lighter text-gray-900
-                      border border-grey-lighter rounded py-3 px-4 mb-3 leading-tight
+                      border border-grey-lighter rounded py-3 px-4 leading-tight
                       focus:outline-none"
                 type="password"
                 placeholder="••••••••••••"
-                required
                 v-model="password">
+        <div v-if="!$v.password.required">Este campo es obligatorio</div>
       </div>
     </div>
     <div class="-mx-2 md:flex md:flex-row-reverse">
       <div class="md:w-1/4 px-3 mb-6 md:mb-0">
+
         <button type="submit"
-                class="group relative w-full flex justify-center py-2 px-4 border
-                        border-transparent text-sm leading-5 font-medium rounded-md
-                        text-white bg-indigo-600 hover:bg-indigo-500
-                        focus:outline-none
-                        focus:border-indigo-700 focus:shadow-outline-indigo
-                        active:bg-indigo-700 transition duration-150 ease-in-out">
+                :disabled="$v.$invalid"
+                :class="{ 'hover:bg-indigo-500': !$v.$invalid,
+                          'cursor-not-allowed': $v.$invalid
+                        }"
+                class="group relative w-full flex justify-center py-2 px-4
+                       border border-transparent text-sm leading-5 font-medium rounded-md
+                       text-white bg-indigo-600
+                       focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo
+                       active:bg-indigo-700 transition duration-200 ease-in-out">
           Crear credencial
         </button>
       </div>
@@ -99,19 +105,37 @@
 </template>
 
 <script>
+import { required } from 'vuelidate/lib/validators';
+
 export default {
   data() {
     return {
-      bank: '',
+      bankName: '',
       holderType: '',
       rut: '',
       password: '',
     };
   },
+  validations: {
+    bankName: {
+      required,
+    },
+    holderType: {
+      required,
+    },
+    rut: {
+      required,
+    },
+    password: {
+      required,
+    },
+  },
   methods: {
     onSubmit() {
+      if (this.$v.$invalid) { return; }
+
       const formData = {
-        institution_id: this.bank,
+        institution_id: this.bankName,
         account_holder_type: this.holderType,
         username: this.rut,
         password: this.password,
