c3f870e3cacee218a6b6bf3fe4b8e2cfb7528931
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2020-04-17 20:09:18 -0400

    fix(link-detail): adjust alert to use the same padding and margin as the others alerts

--- src/components/links/link-detail.vue
+++ src/components/links/link-detail.vue
@@ -1,20 +1,20 @@
 <template>
 <div>
-  <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3
-              shadow-md mb-6"
-        role="alert">
-    <div class="flex">
-      <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
-      <div>
-        <p class="font-bold">Credenciales guardadas con éxito</p>
-        <p class="text-sm">
-          Está información solo será mostrada una vez.
-          Asegurate de copiar el <b>Access Token</b>
-          </p>
+  <div class="bg-white shadow overflow-hidden sm:rounded-lg border-1">
+    <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-3 py-3
+                shadow-md sm:mx-6 mt-4 mx-4"
+          role="alert">
+      <div class="flex">
+        <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+        <div>
+          <p class="font-bold">Credenciales guardadas con éxito</p>
+          <p class="text-sm">
+            Está información solo será mostrada una vez.
+            Asegurate de copiar el <b>Access Token</b>
+            </p>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="bg-white shadow overflow-hidden sm:rounded-lg border-1">
     <div class="px-4 py-5 border-b border-gray-200 sm:px-6">
       <h3 class="text-lg leading-6 font-medium text-gray-900">
         Información de las credenciales
