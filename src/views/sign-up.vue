a26bf314f0be13426bc0374ce801493c12ab315c
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2020-04-19 17:06:35 -0400

    feat(sign-up): add alert if fintoc is unable to process request

--- src/views/sign-up.vue
+++ src/views/sign-up.vue
@@ -9,6 +9,20 @@
       Registrate acá
     </h2>
     <div class="w-full">
+      <div class="bg-red-100 border-t-4 border-red-500 rounded-b text-red-900 px-4 py-3
+                  shadow-md mb-4 mt-2"
+          role="alert"
+          v-if='showFormError'>
+        <div class="flex">
+          <div class="py-1"><svg class="fill-current h-6 w-6 text-red-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+          <div>
+            <p class="font-bold">Tuvimos un problema con el registro</p>
+            <p class="text-sm">
+              Si el problema persiste escríbenos a sos@fintoc.com. Respondemos muy rápido
+            </p>
+          </div>
+        </div>
+      </div>
       <form @submit.prevent="onSubmit" method="POST">
         <input
             type="email"
@@ -86,6 +100,7 @@ export default {
       name: 'remove-field',
       email: '',
       password: '',
+      showFormError: false,
     };
   },
   methods: {
@@ -99,6 +114,8 @@ export default {
       };
       this.$store.dispatch('signUp', formData).then(() => {
         this.afterSuccess();
+      }).catch(() => {
+        this.showFormError = true;
       });
     },
     afterSuccess() {
