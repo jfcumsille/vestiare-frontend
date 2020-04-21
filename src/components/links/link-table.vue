e26a547620c089d8a10d9432fc857c5fa2dc986c
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2020-04-21 01:00:22 -0400

    feat(link-table): show rut using rut filter

--- src/components/links/link-table.vue
+++ src/components/links/link-table.vue
@@ -38,7 +38,7 @@
         </div>
       </td>
       <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-        <div class="text-lg leading-5 text-gray-900">{{ link.rut }}</div>
+        <div class="text-lg leading-5 text-gray-900">{{ link.rut | rutFilter}}</div>
       </td>
       <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
         <div class="text-lg leading-5 text-gray-900">
