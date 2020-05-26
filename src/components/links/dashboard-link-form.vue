7d5544b1ce9c6d8bcbca1d30058af7e24c2cf1b1
fdominguezc <fdominguezc@uc.cl>
2020-05-25 22:35:04 -0400

    refactor(newLink):  refactor components to allow widget component

---
+++ src/components/links/dashboard-link-form.vue
@@ -0,0 +1,68 @@
+<template>
+<div>
+  <link-form v-if="showForm"
+    @createSuccess='onCreateSuccess'
+    :createdThrough="'dashboard'"
+    :submitAction="'createUserLinkFromDashboard'"
+    :headers="formHeaders">
+  </link-form>
+  <link-detail v-if="!showForm"
+      :bank='bank'
+      :holderType='holderType'
+      :numberOfAccounts='numberOfAccounts'
+      :linkToken='linkToken'>
+  </link-detail>
+</div>
+</template>
+
+<script>
+
+import LinkDetail from './link-detail.vue';
+import LinkForm from './link-form.vue';
+import { findBankByCode } from '../../banks-helper';
+
+export default {
+  data() {
+    return {
+      showForm: true,
+      bank: null,
+      holderType: '',
+      numberOfAccounts: '',
+      linkToken: '',
+    };
+  },
+  mounted() {
+    window.analytics.page('New dashboard Link');
+  },
+  components: {
+    LinkForm,
+    LinkDetail,
+  },
+  computed: {
+    formHeaders() {
+      return this.$store.getters.authHeaders;
+    },
+  },
+  methods: {
+    showLinkDetail(responseData) {
+      this.bank = responseData.bank;
+      this.holderType = responseData.holderType;
+      this.numberOfAccounts = responseData.numberOfAccounts;
+      this.linkToken = responseData.linkToken;
+      this.showForm = false;
+    },
+    updateStep(newStep) {
+      this.currentStep = newStep;
+    },
+    onCreateSuccess(response) {
+      const newLinkData = {
+        bank: findBankByCode(response.data.institution.id),
+        numberOfAccounts: response.data.accounts.length,
+        linkToken: response.data.link_token,
+      };
+
+      this.showLinkDetail(newLinkData);
+    },
+  },
+};
+</script>
