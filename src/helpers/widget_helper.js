231b4aec208ea7db0ed838f2725f0e8eee95e3f8
fdominguezc <fdominguezc@uc.cl>
2020-06-08 18:10:06 -0400

    feat(widget): use a stronger validation

--- src/helpers/widget_helper.js
+++ src/helpers/widget_helper.js
@@ -1,3 +1,5 @@
+const errorObject = require('./error_object');
+
 function getValidUrl(url) {
   // Adapted from https://stackoverflow.com/a/11300985
   let newUrl = window.decodeURIComponent(url);
@@ -16,8 +18,38 @@ function getValidUrl(url) {
 
 function validateQueryParams(params) {
   const requiredParams = ['webhook_url', 'public_key', 'holder_type', 'redirect_to'];
+  const holderTypes = ['individual', 'business'];
 
-  return requiredParams.every((key) => Object.prototype.hasOwnProperty.call(params, key));
+  const result = { valid: true, error: errorObject };
+
+  requiredParams.forEach((key) => {
+    if (!Object.prototype.hasOwnProperty.call(params, key)) {
+      result.valid = false;
+      result.error.type = 'invalid_request_error';
+      result.error.code = 'missing_parameter';
+      result.error.message = `Missing required param: ${key}`;
+      result.error.param = key;
+    }
+
+    if (result.valid && (params[key] === null || params[key] === '')) {
+      result.valid = false;
+      result.error.type = 'invalid_request_error';
+      result.error.code = 'empty_string';
+      result.error.message = `Empty string is invalid: ${key}`;
+      result.error.param = key;
+    }
+  });
+
+  if (result.valid && !holderTypes.includes(params.holder_type)) {
+    result.valid = false;
+    result.error.type = 'invalid_request_error';
+    result.error.code = 'invalid_holder_type';
+    result.error.message = `Invalid holder_type: ${params.holder_type}.
+      An account holder type can be either individual or business.`;
+    result.error.param = 'holder_type';
+  }
+
+  return result;
 }
 
 function redirectUrl(params) {
