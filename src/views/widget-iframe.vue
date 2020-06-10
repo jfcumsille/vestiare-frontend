133d37ad199544cb83b079e568e7f37ac60cecc3
fdominguezc <fdominguezc@uc.cl>
2020-06-10 14:32:50 -0400

    feat(): create widget-iframe component

---
+++ src/views/widget-iframe.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <main class="h-screen">
+    <widget-link-form></widget-link-form>
+  </main>
+</div>
+</template>
+<script>
+
+import WidgetLinkForm from '../components/links/widget-link-form.vue';
+
+export default {
+  data() {
+    return {
+    };
+  },
+  components: {
+    WidgetLinkForm,
+  },
+};
+</script>
