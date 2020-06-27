3f969692bf5a3588847350fe6b143b3aec937dad
fdominguezclaro <fdominguezc@uc.cl>
2020-06-27 02:06:47 -0400

    feat(api): add subscriptions update endpoint

--- src/api/subscriptions.js
+++ src/api/subscriptions.js
@@ -2,7 +2,7 @@ import axiosInstance from './config';
 
 function create(accountId, customerId, linkToken, authenticationHeader) {
   return axiosInstance
-    .post(`/internal/v1/accounts/${accountId}/subscription`, {
+    .post(`/internal/v1/accounts/${accountId}/subscriptions`, {
       customerId,
       linkToken,
     }, {
@@ -10,6 +10,14 @@ function create(accountId, customerId, linkToken, authenticationHeader) {
     });
 }
 
+function update(accountId, subscriptionId, data, authenticationHeader) {
+  return axiosInstance
+    .patch(`/internal/v1/accounts/${accountId}/subscriptions/${subscriptionId}`, data, {
+      headers: authenticationHeader,
+    });
+}
+
 export default {
   create,
+  update,
 };
