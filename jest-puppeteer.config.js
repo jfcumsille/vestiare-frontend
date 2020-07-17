5c3e969e7fc775cabf551ed8778a94e024f93f51
Diego Fernández <difernandez@uc.cl>
2020-07-17 18:04:41 -0400

    chore(): start test server in separate step in circleci

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,8 @@
-module.exports = {
-  server: {
-    command: 'yarn serve --port=4444',
-    port: 4444,
-  },
-};
+if (process.env.SKIP_JEST_PUPPETEER_SERVER !== 'true') {
+  module.exports = {
+    server: {
+      command: 'yarn serve --port=4444',
+      port: 4444,
+    },
+  };
+}
