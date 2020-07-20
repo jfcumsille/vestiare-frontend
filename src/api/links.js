93129d6e69219d8750a2d597b1f80b126b73e1b6
Lukas Zorich <lukas.zorich@gmail.com>
2020-07-20 15:08:45 -0400

    fix(async_links): regenerate link token bug

--- src/api/links.js
+++ src/api/links.js
@@ -4,14 +4,14 @@ function get(linkId, headers) {
   return axiosInstance.get(`/internal/v1/links/${linkId}`, { headers });
 }
 
-function regenerateAccessToken(linkId, headers) {
+function regenerateLinkToken(linkId, headers) {
   return axiosInstance
-    .post(`/internal/v1/links/${linkId}/regenerate_access_token`, {
+    .post(`/internal/v1/links/dashboard/${linkId}/regenerate_link_token`, {}, {
       headers,
     });
 }
 
 export default {
   get,
-  regenerateAccessToken,
+  regenerateLinkToken,
 };
