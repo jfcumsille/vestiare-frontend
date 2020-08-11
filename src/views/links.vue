1ee0e4ae3394fcbd544a5577bceb85a618c2f46c
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2020-08-11 19:35:58 -0400

    fix(): rename credentials to links

--- src/views/links.vue
+++ src/views/links.vue
@@ -3,7 +3,7 @@
   <header class="bg-white">
     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
       <h1 class="text-3xl font-medium leading-tight text-gray-900">
-        Credenciales activas
+        Links activos
       </h1>
     </div>
   </header>
