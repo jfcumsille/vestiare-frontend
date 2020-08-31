9af77bf5c3e679e96a0aa264e89d691f1637a07f
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2020-08-30 22:12:00 -0400

    fix(style): recover password

--- src/views/change-password.vue
+++ src/views/change-password.vue
@@ -3,11 +3,11 @@
   <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
     <spinner v-if='showSpinner'></spinner>
     <div class="max-w-md w-full">
-      <img class="mx-auto h-16 w-auto" src="../assets/images/fintoc-logo.png" alt="fintoc" />
+      <img class="mx-auto h-12 w-auto" src="../assets/images/fintoc-logo.png" alt="fintoc" />
       <div>
-        <h2 class="mt-6 text-center text-4xl leading-9 font-regular text-gray-900">
+        <h1 class="mt-6 text-center text-4xl leading-9 font-regular text-gray-900">
           Escribe tu nueva contraseña
-        </h2>
+        </h1>
       </div>
       <div class="bg-red-100 border-t-4 border-red-500 rounded-b text-red-900 px-4 py-3
                       shadow-md mb-4 mt-4"
@@ -34,30 +34,18 @@
         <input
             type="password"
             class="border border-gray-400 placeholder-gray-500 text-gray-900 w-full p-3
-                  rounded-b-md focus:outline-none border border-t-0"
+                  rounded-b-md focus:outline-none border-t-0"
             name="passwordConfirmation"
             placeholder="Confirma contraseña"
             v-model.trim="$v.passwordConfirmation.$model">
 
         <div class="mt-6">
-          <button type="submit"
-                  class="group relative w-full flex justify-center py-3 px-4 border
-                         border-transparent text-sm leading-5 font-medium rounded-md
-                         text-white bg-indigo-600 focus:outline-none focus:border-indigo-700
-                         focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150
-                         ease-in-out"
-                  :class="{ 'hover:bg-indigo-500': !$v.$invalid,
-                            'cursor-not-allowed': $v.$invalid }">
-            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-              <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out
-                          duration-150" fill="currentColor" viewBox="0 0 20 20">
-                <path fill-rule="evenodd"
-                      d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0
-                        01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
-              </svg>
-            </span>
-            Confirmar
-          </button>
+            <action-button
+              :has-lock-icon="true"
+              :invalid-form="$v.$invalid"
+              >
+              Recuperar contraseña
+            </action-button>
         </div>
 
         <div class="mt-6 flex items-center justify-between">
@@ -77,8 +65,9 @@
 </template>
 
 <script>
-import { required } from 'vuelidate/lib/validators';
+import { required, minLength } from 'vuelidate/lib/validators';
 import Spinner from '../components/spinner.vue';
+import actionButton from '../components/actionButton.vue';
 
 export default {
   data() {
@@ -91,6 +80,7 @@ export default {
     };
   },
   components: {
+    actionButton,
     Spinner,
   },
   methods: {
@@ -125,9 +115,11 @@ export default {
   validations: {
     password: {
       required,
+      minLength: minLength(8),
     },
     passwordConfirmation: {
       required,
+      minLength: minLength(8),
     },
   },
 };
