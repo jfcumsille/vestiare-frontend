252b575a463f0cceb672d9404ed24f3a47280ac4
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2020-08-30 20:29:25 -0400

    fix(): remove header from views

--- src/views/links.vue
+++ src/views/links.vue
@@ -1,41 +1,32 @@
 <template>
 <div class="bg-gray-200">
-  <header class="bg-white">
-    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-medium leading-tight text-gray-900">
-        Links activos
-      </h1>
-    </div>
-  </header>
   <main class="h-screen">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 relative">
+    <div class="max-w-full mx-auto p-6 lg:p-8 relative">
       <spinner v-if="shouldShowSpinner"></spinner>
-      <div class="px-4 py-6 sm:px-0">
-        <div class="flex flex-col">
-          <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-            <div class="align-middle inline-block min-w-full overflow-hidden
-                        sm:rounded-md border-gray-200">
-              <link-table v-if="shouldShowTable"></link-table>
-              <div v-if="!shouldShowTable" class="text-center">
-                <h1 class="text-4xl mt-4">
-                  Todavía no agregaste ninguna credencial 👀
-                </h1>
-              </div>
+      <div class="flex flex-col">
+        <div class="overflow-x-auto">
+          <div class="align-middle inline-block min-w-full overflow-hidden
+                      sm:rounded-md border-gray-200">
+            <link-table v-if="shouldShowTable"></link-table>
+            <div v-if="!shouldShowTable" class="text-center">
+              <h1 class="text-4xl mt-4">
+                Todavía no agregaste ninguna credencial 👀
+              </h1>
             </div>
-            <div class="mt-4 text-right">
-              <router-link to="/links/new?holder-type=individual"
-                :class="{ 'text-xs': shouldShowTable }"
-                class="px-2 py-1 inline-flex text-l leading-5 font-semibold rounded-md bg-gray-200
-                       text-gray-900 hover:bg-gray-300">
-                <font-awesome-icon icon="plus" class="mt-1 mr-1"/> Nuevo link personas
-              </router-link>
-              <router-link to="/links/new?holder-type=business"
-                :class="{ 'text-xs': shouldShowTable }"
-                class="px-2 py-1 inline-flex text-l leading-5 font-semibold rounded-md bg-gray-200
-                       text-gray-900 hover:bg-gray-300">
-                <font-awesome-icon icon="plus" class="mt-1 mr-1"/> Nuevo link empresas
-              </router-link>
-            </div>
+          </div>
+          <div class="mt-4 text-right">
+            <router-link to="/links/new?holder-type=individual"
+              :class="{ 'text-xs': shouldShowTable }"
+              class="px-2 py-1 inline-flex text-l leading-5 font-semibold rounded-md bg-gray-200
+                      text-gray-900 hover:bg-gray-300">
+              <font-awesome-icon icon="plus" class="mt-1 mr-1"/> Nuevo link personas
+            </router-link>
+            <router-link to="/links/new?holder-type=business"
+              :class="{ 'text-xs': shouldShowTable }"
+              class="px-2 py-1 inline-flex text-l leading-5 font-semibold rounded-md bg-gray-200
+                      text-gray-900 hover:bg-gray-300">
+              <font-awesome-icon icon="plus" class="mt-1 mr-1"/> Nuevo link empresas
+            </router-link>
           </div>
         </div>
       </div>
