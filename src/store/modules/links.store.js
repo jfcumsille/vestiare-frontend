2ad968a3158deb104e8d457c93395ddee77db0db
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2021-01-30 21:00:50 -0800

    fix(links): fix style

--- src/store/modules/links.store.js
+++ src/store/modules/links.store.js
@@ -21,9 +21,11 @@ const mutations = {
       mode: link.mode,
     }));
   },
+
   removeUserLink(state, linkId) {
     state.userLinks = state.userLinks.filter((link) => link.linkId !== linkId);
   },
+
   updateLoading(state, status) {
     state.loading = status;
   },
@@ -43,6 +45,7 @@ const actions = {
       }).catch((error) => reject(error));
     });
   },
+
   destroyUserLink(context, linkId) {
     const url = `/v1/links/${linkId}`;
     return axiosAuth.delete(url, { headers: this.getters.authHeaders })
@@ -53,10 +56,12 @@ const actions = {
       })
       .catch((error) => console.log(error));
   },
+
   createUserLinkFromWidget(context, payload) {
     const url = 'internal/v1/links/widget';
     return axiosAuth.post(url, payload.formData, { headers: { ...payload.headers } });
   },
+
   createUserLinkFromDashboard(context, payload) {
     const url = 'internal/v1/links/dashboard';
     return axiosAuth.post(url, payload.formData, { headers: { ...payload.headers } })
@@ -65,6 +70,7 @@ const actions = {
         return response;
       });
   },
+
   sendNewLinkRequest(context, formData) {
     return new Promise((resolve, reject) => {
       const url = '/v1/user_credentials';
