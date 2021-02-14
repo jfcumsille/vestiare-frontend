74b111eb26e043fa6765b98433ae6021f1aa8006
Ignacio Acosta <iaacosta@uc.cl>
2021-02-13 21:27:27 -0300

    improvement(action-button): add option to customize tag, icon and attributes

--- src/components/actionButton.vue
+++ src/components/actionButton.vue
@@ -1,14 +1,18 @@
 <template>
-  <button type="submit"
+  <component :is="tag"
+          v-bind="$attrs"
+          type="submit"
           class="group relative w-full flex justify-center py-4 px-4 border
                   border-transparent text-sm leading-5 font-medium rounded-md
                   text-white fintoc-blue focus:outline-none transition duration-150
                   ease-in-out focus:opacity-50"
           :class="{ 'hover:opacity-50': !invalidForm, 'cursor-not-allowed': invalidForm }">
     <span
-      v-if='hasLockIcon'
-      class="absolute left-0 inset-y-0 flex items-center pl-3">
-      <svg class="h-5 w-5 text-white opacity-25 transition ease-in-out duration-150"
+      class="absolute left-0 inset-y-0 flex items-center
+             pl-3 text-white opacity-25 transition
+             ease-in-out duration-150">
+      <svg v-if="hasLockIcon"
+           class="h-5 w-5"
            fill="currentColor"
            viewBox="0 0 20 20">
         <path fill-rule="evenodd"
@@ -16,15 +20,19 @@
                  01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
               clip-rule="evenodd" />
       </svg>
+      <div v-else class="h-5 w-5">
+        <slot name="icon" />
+      </div>
     </span>
     <slot>
     </slot>
-  </button>
+  </component>
 </template>
 
 <script>
 export default {
   props: {
+    tag: { type: String, default: 'button' },
     hasLockIcon: Boolean,
     invalidForm: Boolean,
   },
