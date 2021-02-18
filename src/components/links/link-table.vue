8df517648f2e919bc83ddc8f156e1a06df5602e1
Juan Pablo Jofré <juanpablo@fintoc.com>
2021-02-17 17:25:37 -0800

    refactor(links/deleteModal): abstract confirm deletion modal on its own component

--- src/components/links/link-table.vue
+++ src/components/links/link-table.vue
@@ -1,85 +1,15 @@
 <template>
 <div>
-  <transition name="background-fade" mode="out-in" >
-    <div v-show="showModal"
-      class="fixed z-10 inset-0 overflow-y-auto">
-      <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block
-                  sm:p-0">
-        <div class="fixed inset-0 transition-opacity">
-          <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-        </div>
-        <!-- This element is to trick the browser into centering the modal contents. -->
-        <span class="hidden sm:inline-block sm:align-middle sm:h-screen">
-        </span>
-        <transition name="modal-fade" mode="out-in" >
-        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden
-                    shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg
-                    sm:w-full"
-            role="dialog"
-            v-show="showModal"
-            aria-modal="true"
-            aria-labelledby="modal-headline">
-          <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-            <div class="sm:flex sm:items-start">
-              <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12
-                          rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                <!-- Heroicon name: exclamation -->
-                <svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732
-                      4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-                  />
-                </svg>
-              </div>
-              <spinner v-if="showSpinner"></spinner>
-              <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
-                  Eliminar Link
-                </h3>
-                <div class="mt-2">
-                  <p class="text-sm leading-5 text-gray-700">
-                    ¿Estas seguro que quieres borrar este link?
-                    El usuario tendrá que hacer login de nuevo en el banco para crearlo.
-                    Esta acción no se puede deshacer.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-            <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
-              <button
-                @click="confirmDestroyLink()"
-                type="button"
-                class="inline-flex justify-center w-full rounded-md border border-transparent px-4
-                      py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm
-                    hover:bg-red-500 focus:outline-none focus:border-red-700
-                      focus:shadow-outline-red transition ease-in-out duration-150 sm:text-sm
-                      sm:leading-5">
-                Eliminar
-              </button>
-            </span>
-            <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
-              <button
-                @click="cancelLinkDeletion()"
-                type="button"
-                class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2
-                    bg-white text-base leading-6 font-medium text-gray-700 shadow-sm
-                    hover:text-gray-500 focus:outline-none focus:border-blue-300
-                      focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm
-                      sm:leading-5">
-                Cancelar
-              </button>
-            </span>
-          </div>
-        </div>
-        </transition>
-      </div>
-    </div>
-  </transition>
+  <link-confirmation-modal v-if="showDeleteModal"
+      :title='"Eliminar Link"'
+      :text='`¿Estás seguro que quieres borrar este link? El usuario tendrá que hacer
+              login de nuevo en el banco para crearlo. Esta acción no se puede deshacer.`'
+      :leftButtonText="'Cancelar'"
+      :rightButtonText="'Eliminar'"
+      :onConfirm="confirmDestroyLink"
+      :onCancel="cancelLinkDeletion"
+      :showSpinner="showSpinner"
+      />
 
   <table class="min-w-full">
     <thead class="bg-gray-100">
@@ -161,13 +91,13 @@
 
 <script>
 import { mapState } from 'vuex';
-import Spinner from '../spinner.vue';
+import LinkConfirmationModal from './link-confirmation-modal.vue';
 
 export default {
   data() {
     return {
       showSpinner: false,
-      showModal: false,
+      showDeleteModal: false,
       linkToDestroy: null,
     };
   },
@@ -179,7 +109,7 @@ export default {
   },
 
   components: {
-    Spinner,
+    LinkConfirmationModal,
   },
 
   methods: {
@@ -188,12 +118,12 @@ export default {
     },
 
     askForLinkDeletion(linkId) {
-      this.showModal = true;
+      this.showDeleteModal = true;
       this.linkToDestroy = linkId;
     },
 
     cancelLinkDeletion() {
-      this.showModal = false;
+      this.showDeleteModal = false;
       this.linkToDestroy = null;
     },
 
@@ -211,7 +141,7 @@ export default {
       this.showSpinner = true;
       await this.destroyLink(this.linkToDestroy);
       this.showSpinner = false;
-      this.showModal = false;
+      this.showDeleteModal = false;
     },
 
     translatedHolderType(holderType) {
