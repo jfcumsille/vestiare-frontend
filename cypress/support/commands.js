9f05e7c99a29cdad48aed3918b549b42f9ff772b
parayab <paulina@fintoc.com>
2022-02-10 13:13:48 -0300

    tests(integration): add movements product test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('openWidget', (params) => {
+  cy.visit(`/widget-iframe?${new URLSearchParams(params)}`);
+  cy.get('#intro-continue-btn').click();
+  cy.get('#bank-btns-container button').first().click();
+});
