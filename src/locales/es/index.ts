b735452336e0f3abdc489a90c3d77a6cfbd09c6d
Daniel Leal <dlleal@uc.cl>
2022-02-21 11:28:01 -0300

    chore: remove unused common element

--- src/locales/es/index.ts
+++ src/locales/es/index.ts
@@ -1,10 +1,8 @@
-import common from './common';
 import counter from './counter';
 import homeView from './views/home';
 
 export default {
   translation: {
-    common,
     counter,
     views: {
       home: homeView,
