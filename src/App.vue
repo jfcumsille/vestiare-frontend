07738aa82d828ec1e4e2ea7bc3edf329c2b0a69e
Daniel Leal <dlleal@uc.cl>
2022-03-03 11:07:36 -0300

    feat: create very simple navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
+import NavBar from '@/components/layout/NavBar.vue';
+
 import { useLocaleStore } from '@/stores/locale';
 
 const $store = useLocaleStore();
+
+const navBarLinks = [
+  {
+    text: 'Links',
+    path: '/links',
+  },
+  {
+    text: 'Webhook Endpoints',
+    path: '/webhook_endpoints',
+  },
+];
 </script>
 
 <template>
+  <NavBar :links="navBarLinks" />
   <router-view :key="$store.language" />
 </template>
---
+++ src/components/layout/NavBar.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+interface Link {
+  text: string
+  path: string
+}
+
+const $props = defineProps<{ links: Link[] }>();
+
+const $route = useRoute();
+
+const selectionClasses = (link: Link) => {
+  if ($route.path === link.path) {
+    return 'text-blue-700';
+  }
+  return 'text-gray-700 hover:text-blue-700';
+};
+</script>
+
+<template>
+  <nav class="bg-white border-gray-200 px-2 py-2.5 rounded">
+    <div class="container flex flex-wrap justify-between items-center mx-auto">
+      <a
+        href="#"
+        class="flex items-center"
+      >
+        <span class="self-center text-xl font-semibold whitespace-nowrap">Fintoc</span>
+      </a>
+      <div class="block w-auto">
+        <ul class="flex mt-4 flex-row space-x-8 text-sm font-medium">
+          <li
+            v-for="link in $props.links"
+            :key="link.path"
+          >
+            <router-link
+              :to="link.path"
+              :class="`block p-0 ${selectionClasses(link)}`"
+            >
+              {{ link.text }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
