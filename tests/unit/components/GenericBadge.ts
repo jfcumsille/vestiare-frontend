6b4c6f7309897c0fadbee0a096ac2b4782eed4c1
Daniel Leal <dlleal@uc.cl>
2022-03-08 16:08:39 -0300

    tests: test the generic badge component

---
+++ tests/unit/components/GenericBadge.ts
@@ -0,0 +1,35 @@
+import { mount } from '@vue/test-utils';
+import GenericBadge from '@/components/GenericBadge.vue';
+
+describe('GenericBadge', () => {
+  it('renders props.text when passed', () => {
+    const text = 'This is a badge';
+    const wrapper = mount(GenericBadge, {
+      props: { text, color: 'blue' },
+    });
+    expect(wrapper.text()).toMatch(text);
+  });
+
+  it('renders a badge with no color when color prop is invalid', () => {
+    const invalidColor = 'this is absolutely invalid as a color';
+    const wrapper = mount(GenericBadge, {
+      props: { text: 'Something', color: invalidColor },
+    });
+    expect(wrapper.classes().some((cls) => cls.includes('bg-'))).toBe(false);
+  });
+
+  const testColorRendering = (color: string) => {
+    it(`renders a ${color} badge when color is ${color}`, () => {
+      const wrapper = mount(GenericBadge, {
+        props: { text: 'Something', color },
+      });
+      expect(wrapper.classes().filter((cls) => cls.includes(color)).length).toBe(2);
+    });
+  };
+
+  const colors = [
+    'blue', 'gray', 'red', 'green', 'yellow', 'indigo', 'purple', 'pink',
+  ];
+
+  colors.forEach((color) => testColorRendering(color));
+});
