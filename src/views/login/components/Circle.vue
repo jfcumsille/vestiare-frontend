dbfeabd52bd72db2f8f1d067e66f80efa447b054
paulamardones <91501325+paulamardones@users.noreply.github.com>
2022-03-14 11:56:01 -0300

    refactor: login & signup images to src/components

--- src/views/login/components/Circle.vue
+++
@@ -1,5 +0,0 @@
-<template>
-<svg width="377" height="377" viewBox="0 0 377 377" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M377 188.5C377 292.606 292.606 377 188.5 377C84.3943 377 0 292.606 0 188.5C0 84.3943 84.3943 0 188.5 0C292.606 0 377 84.3943 377 188.5Z" fill="#F2F4FF" fill-opacity="0.5"/>
-</svg>
-</template>
