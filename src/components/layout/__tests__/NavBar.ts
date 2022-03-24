4997dd41569b8e578f96e9a71a185725f639a020
paulamardones <91501325+paulamardones@users.noreply.github.com>
2022-03-24 01:12:11 -0300

    update: nav bar test

--- src/components/layout/__tests__/NavBar.ts
+++ src/components/layout/__tests__/NavBar.ts
@@ -5,58 +5,63 @@ import { setActivePinia, createPinia } from 'pinia';
 import { mount } from '@vue/test-utils';
 import { useUserStore } from '@/stores/user';
 import { setupLocales } from '@/locales';
+import router from '@/router/index';
 import NavBar from '../NavBar.vue';
 
+const getWrapper = () => {
+  const wrapper = mount(NavBar, {
+    global: {
+      plugins: [router],
+    },
+  });
+  return wrapper;
+};
 describe('NavBar', () => {
   beforeEach(() => {
     setActivePinia(createPinia());
     setupLocales();
   });
 
-  /*
   describe('when logged in', () => {
-    it('shows internal nav bar links', () => {
-      const userStore = useUserStore();
+    let userStore;
+    beforeEach(() => {
+      userStore = useUserStore();
       userStore.auth.id = 'id';
       userStore.auth.email = 'email';
       userStore.auth.authenticationToken = 'authToken';
-      const wrapper = mount(NavBar);
+    });
+    it('shows internal nav bar links', () => {
+      const wrapper = getWrapper();
       const navBarInternalLinks = wrapper.find('[data-test="navBarInternalLinks"]');
       expect(navBarInternalLinks.exists()).toBe(true);
       const navBarPublicLinks = wrapper.find('[data-test="navBarPublicLinks"]');
       expect(navBarPublicLinks.exists()).toBe(false);
     });
     it('redirects to \'/\' when clicking the fintoc logo', () => {
-      const userStore = useUserStore();
-      userStore.auth.id = 'id';
-      userStore.auth.email = 'email';
-      userStore.auth.authenticationToken = 'authToken';
-      const wrapper = mount(NavBar);
+      const wrapper = getWrapper();
       const fintocLogo = wrapper.find('[data-test="fintocLogo"]');
       expect(fintocLogo.exists()).toBe(true);
       expect(fintocLogo.attributes('href')).toEqual('/');
     });
   });
-  */
 
   describe('when not logged in', () => {
-    it('shows public nav bar links', () => {
-      const userStore = useUserStore();
+    let userStore;
+    beforeEach(() => {
+      userStore = useUserStore();
       userStore.auth.id = '';
       userStore.auth.email = '';
       userStore.auth.authenticationToken = '';
-      const wrapper = mount(NavBar);
+    });
+    it('shows public nav bar links', () => {
+      const wrapper = getWrapper();
       const navBarPublicLinks = wrapper.find('[data-test="navBarPublicLinks"]');
       expect(navBarPublicLinks.exists()).toBe(true);
       const navBarInternalLinks = wrapper.find('[data-test="navBarInternalLinks"]');
       expect(navBarInternalLinks.exists()).toBe(false);
     });
     it('fintoc logo redirects to fintoc.com', () => {
-      const userStore = useUserStore();
-      userStore.auth.id = '';
-      userStore.auth.email = '';
-      userStore.auth.authenticationToken = '';
-      const wrapper = mount(NavBar);
+      const wrapper = getWrapper();
       const fintocLogo = wrapper.find('[data-test="fintocLogo"]');
       expect(fintocLogo.exists()).toBe(true);
       expect(fintocLogo.attributes('href')).toEqual('https://fintoc.com');
