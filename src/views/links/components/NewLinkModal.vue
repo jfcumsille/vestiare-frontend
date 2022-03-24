6f770aa151fd159ed97c9ba0f99ea04178d56d45
Daniel Leal <dlleal@uc.cl>
2022-03-24 17:12:58 -0300

    refactor: move structure for link created modal into generic modal

--- src/views/links/components/NewLinkModal.vue
+++ src/views/links/components/NewLinkModal.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useTranslation } from '@/locales';
-import GenericCross from '@/components/GenericCross.vue';
+import GenericModal from '@/components/GenericModal.vue';
 
 const $t = useTranslation('views.links.creation.modal');
 
@@ -16,46 +16,18 @@ const close = () => {
 </script>
 
 <template>
-  <div class="fixed left-0 top-0 z-50 w-screen h-screen">
-    <div
-      class="
-        flex overflow-y-auto overflow-x-hidden justify-center items-center
-        md:h-full bg-black/30
-      "
-    >
-      <div class="px-4 w-full max-w-2xl h-full md:h-auto mx-auto">
-        <div class="relative bg-white rounded-lg shadow">
-          <div class="flex justify-between items-start p-5 rounded-t border-b">
-            <h3 class="text-xl font-semibold text-gray-900 lg:text-2xl">
-              {{ $t('title') }}
-            </h3>
-            <button
-              class="
-                text-gray-400 bg-transparent hover:bg-gray-200
-                hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto
-                inline-flex items-center
-              "
-              @click="close"
-            >
-              <GenericCross
-                class="w-5 h-5"
-                viewBox="0 0 20 20"
-              />
-            </button>
-          </div>
-          <div class="p-6 space-y-6">
-            <!-- eslint-disable vue/no-v-html -->
-            <p
-              class="text-base leading-relaxed text-gray-500"
-              v-html="$t('contentHTML')"
-            />
-            <!-- eslint-enable vue/no-v-html -->
-            <p class="text-base leading-relaxed text-black font-bold">
-              {{ props.linkToken }}
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <GenericModal
+    :title="$t('title')"
+    @close="close"
+  >
+    <!-- eslint-disable vue/no-v-html -->
+    <p
+      class="text-base leading-relaxed text-gray-500"
+      v-html="$t('contentHTML')"
+    />
+    <!-- eslint-enable vue/no-v-html -->
+    <p class="text-base leading-relaxed text-black font-bold">
+      {{ props.linkToken }}
+    </p>
+  </GenericModal>
 </template>
