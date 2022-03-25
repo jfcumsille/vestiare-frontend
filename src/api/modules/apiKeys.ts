8630f9eb6d77f2a8d7f3d57c27e5c7bd9b179961
Daniel Leal <dlleal@uc.cl>
2022-03-25 11:47:26 -0300

    refactor: move api keys base path to the API wrapper file

--- src/api/modules/apiKeys.ts
+++ src/api/modules/apiKeys.ts
@@ -1,18 +1,19 @@
 import client from '@/api/client';
 import { APIKey } from '@/interfaces/entities/apiKeys';
-import { API_KEYS_BASE_PATH } from '@/constants/api';
+
+export const BASE_PATH = '/v1/api_keys';
 
 export const list = async (): Promise<Array<APIKey>> => {
-  const response = await client.get(API_KEYS_BASE_PATH);
+  const response = await client.get(BASE_PATH);
   return response.data;
 };
 
 export const create = async (): Promise<void> => {
-  await client.post(API_KEYS_BASE_PATH);
+  await client.post(BASE_PATH);
 };
 
 export const destroy = async (
   keyId: string,
 ): Promise<void> => {
-  await client.delete(`${API_KEYS_BASE_PATH}/${keyId}`);
+  await client.delete(`${BASE_PATH}/${keyId}`);
 };
