229e452d1031a24e288c7010386260298eb2088f
Daniel Leal <dlleal@uc.cl>
2022-03-25 11:51:39 -0300

    refactor: move every base path to its wrapper file

--- src/api/modules/authentication.ts
+++ src/api/modules/authentication.ts
@@ -2,24 +2,27 @@ import client from '@/api/client';
 import { User } from '@/interfaces/entities/user';
 import { LogInOptions, SignUpOptions } from '@/interfaces/options/account';
 
+export const BASE_USERS_PATH = '/internal/v1/users';
+export const BASE_SESSIONS_PATH = '/internal/v1/sessions';
+
 export const logIn = async ({ email, password, token }: LogInOptions): Promise<User> => {
-  const response = await client.post('/internal/v1/sessions', { email, password, token });
+  const response = await client.post(BASE_SESSIONS_PATH, { email, password, token });
   return response.data;
 };
 
 export const signUp = async ({
   email, password, token, name, lastName, company, country,
 }: SignUpOptions): Promise<User> => {
-  const response = await client.post('/internal/v1/users', {
+  const response = await client.post(BASE_USERS_PATH, {
     email, password, token, name, lastName, company, country,
   });
   return response.data;
 };
 
 export const sendConfirmationEmail = async (email: string) => {
-  await client.post('/internal/v1/users/confirmation', { email });
+  await client.post(`${BASE_USERS_PATH}/confirmation`, { email });
 };
 
 export const sendResetPasswordEmail = async (email: string) => {
-  await client.post('/internal/v1/users/password', { email });
+  await client.post(`${BASE_USERS_PATH}/password`, { email });
 };
--- src/api/modules/user.ts
+++ src/api/modules/user.ts
@@ -1,7 +1,9 @@
 import client from '@/api/client';
 import { User } from '@/interfaces/entities/user';
 
+export const BASE_PATH = '/internal/v1/users';
+
 export const get = async (userId: string): Promise<User> => {
-  const response = await client.get(`/internal/v1/users/${userId}`);
+  const response = await client.get(`${BASE_PATH}/${userId}`);
   return response.data;
 };
