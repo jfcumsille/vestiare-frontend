0601101c6a78fef28bb22d937a79fd1a53600cdc
Daniel Leal <dlleal@uc.cl>
2022-03-25 13:08:37 -0300

    feat: start adding the form components

---
+++ src/components/GenericTextArea.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue: string,
+  label?: string,
+  placeholder?: string,
+}>();
+
+const emit = defineEmits<{(e: 'update:modelValue', value: string): void}>();
+
+const onInput = ($event: Event) => {
+  emit('update:modelValue', ($event.target as HTMLInputElement).value);
+};
+</script>
+
+<template>
+  <label class="block">
+    <span
+      v-if="props.label"
+      data-test="label"
+      class="font-medium text-sec-cap-txt-color"
+    >
+      {{ props.label }}
+    </span>
+    <textarea
+      data-test="textarea"
+      rows="4"
+      class="
+        block w-full px-3 py-2 bg-white border border-slate-300 rounded-md
+        text-sm text-body-txt-color shadow-sm placeholder-slate-400 focus:outline-none
+        focus:border-primary-main focus:ring-1 focus:ring-primary-main resize-none
+      "
+      :placeholder="props.placeholder"
+      :value="props.modelValue"
+      v-bind="$attrs"
+      @input="onInput"
+    />
+  </label>
+</template>
--- src/views/webhookEndpoints/components/WebhookEndpointCreationModal.vue
+++ src/views/webhookEndpoints/components/WebhookEndpointCreationModal.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import GenericModal from '@/components/GenericModal.vue';
+import GenericInput from '@/components/GenericInput.vue';
+import GenericTextArea from '@/components/GenericTextArea.vue';
+import { ref } from 'vue';
+
+const url = ref('');
+const description = ref('');
 
 const emit = defineEmits<{ (e: 'close'): void }>();
 </script>
@@ -9,6 +15,15 @@ const emit = defineEmits<{ (e: 'close'): void }>();
     title="Create Webhook Endpoint"
     @close="emit('close')"
   >
-    A
+    <GenericInput
+      v-model="url"
+      label="Webhook URL"
+      placeholder="https://your.backend/webhook"
+    />
+    <GenericTextArea
+      v-model="description"
+      label="Description"
+      placeholder="Optional description..."
+    />
   </GenericModal>
 </template>
