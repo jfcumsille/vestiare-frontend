b808c16bcef49dba467be63c98091a7deb27bd36
paulamardones <91501325+paulamardones@users.noreply.github.com>
2022-03-25 11:40:30 -0300

    refactor: add constant API_KEYS_BASE_PATH

--- src/api/modules/apiKeys.ts
+++ src/api/modules/apiKeys.ts
@@ -1,17 +1,18 @@
 import client from '@/api/client';
 import { APIKey } from '@/interfaces/entities/apiKeys';
+import { API_KEYS_BASE_PATH } from '@/constants/api';
 
 export const list = async (): Promise<Array<APIKey>> => {
-  const response = await client.get('/v1/api_keys');
+  const response = await client.get(API_KEYS_BASE_PATH);
   return response.data;
 };
 
 export const create = async (): Promise<void> => {
-  await client.post('/v1/api_keys');
+  await client.post(API_KEYS_BASE_PATH);
 };
 
 export const destroy = async (
   keyId: string,
 ): Promise<void> => {
-  await client.delete(`/v1/api_keys/${keyId}`);
+  await client.delete(`${API_KEYS_BASE_PATH}/${keyId}`);
 };
