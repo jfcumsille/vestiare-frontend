44b93d89fa0c3a6547e8318cffafac0070f9019a
paulamardones <91501325+paulamardones@users.noreply.github.com>
2022-03-26 00:40:55 -0300

    add bank and fiscal svgs

---
+++ src/assets/svg/institutions/banks/HsbcIcon.vue
@@ -0,0 +1,9 @@
+<template>
+<svg width="72" height="72" viewBox="0 0 72 72" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M53.927 18H17.9756V53.9514H53.927V18Z" fill="#F5F5F5"/>
+<path d="M54.0234 53.9514L71.9991 35.9757L54.0234 18V53.9514Z" fill="#DB0011"/>
+<path d="M17.9757 53.9514L0 35.9757L17.9757 18V53.9514Z" fill="#DB0011"/>
+<path d="M54.0242 18L35.9513 35.9757L17.9756 18H54.0242Z" fill="#DB0011"/>
+<path d="M17.9756 53.9514L35.9513 35.9757L54.0242 53.9514H17.9756Z" fill="#DB0011"/>
+</svg>
+</template>
