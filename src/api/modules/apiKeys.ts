5ef8d27dde2b943ab6308c1e6b9e4ab6fa85fc53
Daniel Leal <dlleal@uc.cl>
2022-04-21 14:35:54 -0400

    feat: move the API keys endpoint to the internal endpoints

--- src/api/modules/apiKeys.ts
+++ src/api/modules/apiKeys.ts
@@ -1,7 +1,7 @@
 import client from '@/api/client';
 import { APIKey } from '@/interfaces/entities/apiKeys';
 
-export const BASE_PATH = '/v1/api_keys';
+export const BASE_PATH = '/internal/v1/api_keys';
 
 export const list = async (): Promise<Array<APIKey>> => {
   const response = await client.get(BASE_PATH);
