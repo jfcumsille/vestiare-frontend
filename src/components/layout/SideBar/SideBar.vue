7ce5da16f4d933723cee2302e15c346fb2edb80a
parayab <paulina@fintoc.com>
2022-04-26 12:27:33 -0400

    feat(sidebar): add mode dropdown

--- src/components/layout/SideBar/SideBar.vue
+++ src/components/layout/SideBar/SideBar.vue
@@ -3,15 +3,29 @@ import { useTranslation } from '@/locales';
 import LinkIcon from '@/assets/svg/LinksIcon.vue';
 import KeyIcon from '@/assets/svg/KeyIcon.vue';
 import WebhooksIcon from '@/assets/svg/WebhooksIcon.vue';
+import { useConfigStore } from '@/stores/config';
+import { Mode } from '@/interfaces/utilities/enums';
+import GenericDropDown from '@/components/GenericDropDown.vue';
 
+const configStore = useConfigStore();
+const selectMode = (mode: string) => { configStore.updateMode(mode as Mode); };
 const $t = useTranslation('sideBar');
-
 </script>
+
 <template>
   <aside
     class="bg-white h-screen flex flex-col w-64 px-4 py-8 overflow-y-auto"
   >
     <ul class="flex flex-col text-center">
+      <GenericDropDown
+        data-test="mode-dropdown"
+        :selected="configStore.mode"
+        :options="[Mode.Live, Mode.Test]"
+        is-color-primary
+        class="my-2 p-1"
+        is-width-full
+        @select="selectMode"
+      />
       <router-link
         data-test="links-link"
         class="flex items-center px-4 py-3 text-primary-main rounded-md hover:text-primary-hover"
