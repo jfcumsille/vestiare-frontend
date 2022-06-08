44b3bd55572b95cc4780f9b4b53a6d81c7bd781e
paulamardones <91501325+paulamardones@users.noreply.github.com>
2022-06-08 18:26:13 -0400

    feat: add apply and amount of filters selected to header

--- src/components/table/utils/TableFilter.vue
+++ src/components/table/utils/TableFilter.vue
@@ -1,14 +1,36 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { onClickOutside } from '@vueuse/core';
 import FilterIcon from '@/assets/svg/FilterIcon.vue';
 import GenericCheckbox from '@/components/GenericCheckbox.vue';
+import GenericButton from '@/components/GenericButton.vue';
+import { ButtonType } from '@/interfaces/utilities/enums';
 import { Filter } from '@/interfaces/utilities/table';
 
 const props = defineProps<{
   label: string,
   filters: Array<Filter<unknown>>,
 }>();
+
+const emit = defineEmits<{(e: 'apply'): void }>();
+
+const filtersAmount = computed(() => {
+  let total = 0;
+  props.filters.forEach((filter) => {
+    if (!filter.checked) {
+      total += 1;
+    }
+  });
+  return total;
+});
+
+const title = computed(() => {
+  const amountText = `(${filtersAmount.value})`;
+  if (filtersAmount.value > 0) {
+    return `${props.label} ${amountText}`;
+  }
+  return props.label;
+});
 
 const opened = ref(false);
 const toggle = () => {
@@ -32,13 +54,13 @@ onClickOutside(tableFilter, () => {
     >
       <FilterIcon class="w-4 h-4" />
       <div>
-        {{ props.label }}
+        {{ title }}
       </div>
     </button>
     <div
       class="
-        absolute z-10 text-base bg-white cursor-pointer mt-3.5
-        shadow-lg rounded-lg
+        absolute text-base bg-white cursor-pointer mt-3.5
+        shadow-lg rounded-lg z-50
       "
       :class="{ 'hidden': !opened }"
     >
@@ -46,9 +68,17 @@ onClickOutside(tableFilter, () => {
         v-for="filter in props.filters"
         :key="filter.name"
         v-model="filter.checked"
-        class="py-4 px-5"
+        class="p-4"
         :text="filter.name"
       />
+      <div class="m-1.5">
+        <GenericButton
+          class="box-border w-full"
+          :type="ButtonType.Primary"
+          text="Apply"
+          @click="() => emit('apply')"
+        />
+      </div>
     </div>
   </div>
 </template>
