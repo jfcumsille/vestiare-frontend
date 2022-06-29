49a85e40569b2c6c6f57c79c8c5d3b709360e565
parayab <paulina@fintoc.com>
2022-06-29 10:49:01 -0400

    feat: add organization users to org store

--- src/stores/organization.ts
+++ src/stores/organization.ts
@@ -2,11 +2,14 @@ import { defineStore, acceptHMRUpdate } from 'pinia';
 import * as api from '@/api';
 import { Nullable } from '@/interfaces/common';
 import { OrganizationFull } from '@/interfaces/entities/organization';
+import { OrganizationUser } from '@/interfaces/entities/organizationUser';
 
 export const useOrganizationStore = defineStore('organization', {
   state: () => ({
     loading: false,
     organization: <Nullable<OrganizationFull>>null,
+    loadingUsers: false,
+    organizationUsers: <Array<OrganizationUser>>[],
   }),
   actions: {
     async loadOrganization() {
@@ -14,6 +17,12 @@ export const useOrganizationStore = defineStore('organization', {
       const organizationData = await api.organization.get();
       this.organization = organizationData;
       this.loading = false;
+    },
+    async loadOrganizationUsers() {
+      this.loadingUsers = true;
+      const organizationUsersData = await api.organizationUsers.get();
+      this.organizationUsers = organizationUsersData;
+      this.loadingUsers = false;
     },
   },
 });
