5744b45578906cac3adfc9fd886a76fa7647d2ec
parayab <paulina@fintoc.com>
2022-07-08 19:02:10 -0400

    feat(invitations): add on mounted api call

--- src/views/invitation/InvitationView.vue
+++ src/views/invitation/InvitationView.vue
@@ -1,14 +1,34 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import * as api from '@/api';
+import { Nullable } from '@/interfaces/common';
+import { OrganizationUser } from '@/interfaces/entities/organizationUser';
+import LoginView from './components/LoginView.vue';
 
 const route = useRoute();
 const token = route.params.token;
-// on mounted check if invitation token is valid
+const organizationUser = ref<Nullable<OrganizationUser>>(null);
+const loading = ref(false);
+
+onMounted(async () => {
+  loading.value = true;
+  try {
+    organizationUser.value = await api.invitations.get(token as string);
+  } finally {
+    loading.value = false;
+  }
+});
 </script>
 <template>
-  <h1>You have been invited to this org with token {{ token }}</h1>
-  <!-- <LoginView v-if="status==='login'" />
-  <ExpiredView v-if="status==='expired'" />
+  <h1 v-if="loading">
+    You have been invited to this org with token {{ token }}
+  </h1>
+  <LoginView
+    v-if="!loading && organizationUser"
+    :organization-user="organizationUser"
+  />
+  <!-- <ExpiredView v-if="status==='expired'" />
   <ManualSignup v-if="status==='signup'" />
   <OrgRole v-if="status==='orgRole'" /> -->
 </template>
