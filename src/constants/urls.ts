384e0c2de9b345a5c50b8b4dd9094f549a0bcacc
paulamardones <91501325+paulamardones@users.noreply.github.com>
2022-11-10 10:37:29 -0300

    feat: show payments in sidebar only for permitted orgs

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -9,6 +9,7 @@ export const DOCS_WEBHOOKS = 'https://docs.fintoc.com/docs/webhooks-walkthrough'
 export const DOCS_API_KEYS = 'https://docs.fintoc.com/docs/api-keys';
 export const DOCS_API_CHANGELOG = 'https://docs.fintoc.com/docs/api-changelog';
 export const DOCS_EVENTS = 'https://docs.fintoc.com/docs/events-types';
+export const DOCS_PAYMENTS = 'https://docs.fintoc.com/reference/payment-intents-object';
 
 export const TERMS_AND_CONDITIONS = 'https://fintoc.com/legal/terminos-y-condiciones-para-desarrolladores#terminos-y-condiciones';
 export const PRIVACY_POLICY = 'https://fintoc.com/legal/politicas-de-privacidad-para-usuarios-finales';
