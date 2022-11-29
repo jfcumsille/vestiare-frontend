9ec6f8c0798780314adc068de136de698b1f8c52
paulamardones <91501325+paulamardones@users.noreply.github.com>
2022-11-29 14:49:15 -0600

    test: update TableHead and TableHeader tests

--- src/components/table/__tests__/TableHead.ts
+++ src/components/table/__tests__/TableHead.ts
@@ -1,15 +1,33 @@
-import { describe, it, expect } from 'vitest';
+import {
+  beforeEach, describe, it, expect, vi,
+} from 'vitest';
+import { setActivePinia } from 'pinia';
+import { createTestingPinia } from '@pinia/testing';
 import { mount } from '@vue/test-utils';
 import TableHead from '@/components/table/TableHead.vue';
 import TableHeader from '@/components/table/TableHeader.vue';
 
+const testingPinia = createTestingPinia({ createSpy: vi.fn });
+
+const getWrapper = () => {
+  const wrapper = mount(TableHead, {
+    global: {
+      plugins: [testingPinia],
+    },
+    slots: {
+      default: [TableHeader],
+    },
+  });
+  return wrapper;
+};
+
 describe('TableHead', () => {
+  beforeEach(() => {
+    setActivePinia(testingPinia);
+  });
+
   it('renders TableHeader in slot when passed TableHeader', () => {
-    const wrapper = mount(TableHead, {
-      slots: {
-        default: [TableHeader],
-      },
-    });
+    const wrapper = getWrapper();
     const tableHeader = wrapper.findComponent(TableHeader);
     expect(tableHeader.exists()).toBe(true);
   });
