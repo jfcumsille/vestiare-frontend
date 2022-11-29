f26030a688a1522c6531180aa7361418c4c91fa3
paulamardones <91501325+paulamardones@users.noreply.github.com>
2022-11-29 12:42:16 -0600

    refactor: LinksTableHead to only open AppliedFilter

--- src/views/links/components/LinksTableHead.vue
+++ src/views/links/components/LinksTableHead.vue
@@ -1,33 +1,15 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
 import { useTranslation } from '@/locales';
 import TableHead from '@/components/table/TableHead.vue';
 import TableHeader from '@/components/table/TableHeader.vue';
 import TableFilter from '@/components/table/utils/TableFilter.vue';
-import { Filter } from '@/interfaces/utilities/table';
-import { getFilterValues } from '@/utils/table';
 
 const $t = useTranslation('views.links.table');
 
-const emit = defineEmits<{(e: 'update', filterValues: Record<string, Array<boolean>>): void }>();
+const emit = defineEmits<{(e: 'open'): void }>();
 
-const activeFilters = ref<Array<Filter<boolean>>>([
-  { name: $t('filters.active.options.valid'), value: true, checked: true },
-  { name: $t('filters.active.options.invalid'), value: false, checked: true },
-]);
-const activeFilterSelectedValues = computed(() => getFilterValues(activeFilters.value));
-
-const passwordFilters = ref<Array<Filter<boolean>>>([
-  { name: $t('filters.password.options.valid'), value: false, checked: true },
-  { name: $t('filters.password.options.invalid'), value: true, checked: true },
-]);
-const passwordFilterSelectedValues = computed(() => getFilterValues(passwordFilters.value));
-
-const applyFilter = () => {
-  emit('update', {
-    active: activeFilterSelectedValues.value,
-    preventRefresh: passwordFilterSelectedValues.value,
-  });
+const openFilter = (value: string) => {
+  emit('open', value);
 };
 </script>
 
@@ -40,15 +22,13 @@ const applyFilter = () => {
     <TableHeader>
       <TableFilter
         :label="$t('headers.password')"
-        :filters="passwordFilters"
-        @apply="applyFilter"
+        @toggle="openFilter('password')"
       />
     </TableHeader>
     <TableHeader>
       <TableFilter
         :label="$t('headers.active')"
-        :filters="activeFilters"
-        @apply="applyFilter"
+        @toggle="openFilter('active')"
       />
     </TableHeader>
     <TableHeader><div /></TableHeader>
