cf5977e23af001e9b23fda5507c618e0b4a381ce
paulamardones <91501325+paulamardones@users.noreply.github.com>
2022-11-29 12:25:00 -0600

    fix: higher z-index for UserOptionsButton
    remove z-index from ApiKeysView

--- src/components/layout/NavBar/UserOptionsButton.vue
+++ src/components/layout/NavBar/UserOptionsButton.vue
@@ -47,7 +47,7 @@ const truncate = (string: string, maxLen: number) => string.substring(0, maxLen)
   <button
     ref="dropdownMenu"
     data-test="nav-bar-profile-settings-link"
-    class="relative cursor-pointer text-primary-main hover:text-primary-hover flex"
+    class="relative cursor-pointer text-primary-main hover:text-primary-hover flex z-50"
     @click="pressUserMenu"
   >
     <PersonIcon class="mr-2 mt-1" />
